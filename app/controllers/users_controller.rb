8f29fa687ac8a3687bb73020902a1351fde976f1
Bapsk <baptiste.kief@gmail.com>
2020-10-23 14:23:41 -0400

    Add friends feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,38 @@
 class UsersController < ApplicationController
 
+  def show
+    @user = User.find(params[:id])
+    @tracked_stocks = @user.stocks
+  end
+
   def my_portfolio
+    @user = current_user
     @tracked_stocks = current_user.stocks
   end
 
   def friends
     @tracked_friends = current_user.friends
+  end
+
+  def search
+    if params[:friend].present?
+      @friends = User.search(params[:friend])
+      @friends = current_user.except_current_user(@friends)
+      if @friends
+        respond_to do |format|
+          format.js { render partial: 'users/friend_results' }
+        end
+      else
+        respond_to do |format|
+          flash.now[:alert] = "Please enter a valid symbol to search"
+          format.js { render partial: 'users/friend_results' }
+        end
+      end
+    else
+      respond_to do |format|
+          flash.now[:alert] = "Please enter a symbol to search"
+          format.js { render partial: 'users/friend_results' }
+        end
+    end
   end
 end
