cec37ff3da30d4856c8e15a61f806a7dbb7cc18b
Bapsk <baptiste.kief@gmail.com>
2020-10-23 11:34:00 -0400

    Add layout friends

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
+
   def my_portfolio
     @tracked_stocks = current_user.stocks
+  end
+
+  def friends
+    @tracked_friends = current_user.friends
   end
 end
